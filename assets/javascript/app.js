ac9a28372bc454a4aa55a38c0856210ae1d7d6c1
dacup72 <dacup72@gmail.com>
2018-01-03 12:45:36 -0700

    Fix errors and created display function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,37 @@
 
 // Data for trivia questions
 var questions = [{
-  question: "What was the first full length CGI movie?",
-  answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
-  correctAnswer: "Toy Story"
+	question: "What was the first full length CGI movie?",
+	answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+	correctAnswer: "Toy Story"
 }, {
-  question: "Which of these is NOT a name of one of the Spice Girls?",
-  answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
-  correctAnswer: "Fred Spice"
+	question: "Which of these is NOT a name of one of the Spice Girls?",
+	answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+	correctAnswer: "Fred Spice"
 }, {
-  question: "Which NBA team won the most titles in the 90s?",
-  answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
-  correctAnswer: "Chicago Bulls"
+	question: "Which NBA team won the most titles in the 90s?",
+	answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+	correctAnswer: "Chicago Bulls"
 }, {
-  question: "Which group released the hit song, \"Smells Like Teen Spirit\"?",
-  answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
-  correctAnswer: "Nirvana"
+	question: "Which group released the hit song, \"Smells Like Teen Spirit\"?",
+	answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
+	correctAnswer: "Nirvana"
 }, {
-  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
-  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
-  correctAnswer: "The Lion King"
+	question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
+	answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+	correctAnswer: "The Lion King"
 }, {
-  question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
-  answers: ["Dice", "Mirror", "Fresh", "Cab"],
-  correctAnswer: "Fresh"
+	question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
+	answers: ["Dice", "Mirror", "Fresh", "Cab"],
+	correctAnswer: "Fresh"
 }, {
-  question: "What was Doug's best friend's name?",
-  answers: ["Skeeter", "Mark", "Zach", "Cody"],
-  correctAnswer: "Skeeter"
+	question: "What was Doug's best friend's name?",
+	answers: ["Skeeter", "Mark", "Zach", "Cody"],
+	correctAnswer: "Skeeter"
 }, {
-  question: "What was the name of the principal at Bayside High in Saved By The Bell?",
-  answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
-  correctAnswer: "Mr.Belding"
+	question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+	answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+	correctAnswer: "Mr.Belding"
 }];
 
 
@@ -43,23 +43,36 @@ var timer = 0;
 // Main game object
 var game = {
 	correct: 0,
-  incorrect: 0,
+	incorrect: 0,
 	counter: 100,
-	
-	// Count down timer for game
-	countdown: function() {
-    game.counter--;
-    $("#counter-number").html(game.counter);
-    if (game.counter === 0) {
-      console.log("TIME UP");
-      game.done();
-    }
-	},
-	
-	// Done function for when game timer reaches 0
-	done: function() {
 
-    
+	// Count down timer for game
+	countdown: function () {
+		game.counter--;
+		$("#counter-number").html(game.counter);
+		if (game.counter === 0) {
+			console.log("TIME UP");
+			game.done();
+		}
+	},
+
+	// Done function for when game timer reaches 0
+	done: function () {
+
+
+	},
+
+	// Displays results to the user and clears the timer
+	result: function () {
+		clearInterval(timer);
+
+		$("#sub-wrapper h2").remove();
+
+		panel.html("<h2>All Done!</h2>");
+		panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+		panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+		panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+	}
 }
 
 
