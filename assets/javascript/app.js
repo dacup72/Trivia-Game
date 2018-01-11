31e11782e4c8af7a3b5ff043c6251b9b9e374727
dacup72 <dacup72@gmail.com>
2018-01-11 16:23:05 -0700

    refactor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -114,6 +114,7 @@ var game = {
 				<h2>All Done!</h2>
 				<h3>Correct Answers: ${this.correct}</h3>
 				<h3>Incorrect Answers: ${this.incorrect}</h3>
+				<h3>Unanswered: ${(questions.length - (this.incorrect + this.correct))}</h3>
 			</div>
 		`);
 		
